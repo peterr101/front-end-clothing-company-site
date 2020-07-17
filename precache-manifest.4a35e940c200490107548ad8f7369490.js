030dbfe830f06d6ee56acea47f3726c9ad5a68f0
Peter Rassias <ubcpeter@hotmail.com>
2020-07-17 13:49:27 -0700

    Updates

---
+++ precache-manifest.4a35e940c200490107548ad8f7369490.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b654a37c3d5e25d84139a3cd9b20fd0e",
+    "url": "/front-end-clothing-company-site/index.html"
+  },
+  {
+    "revision": "2aeee8a1f69c1e93e28d",
+    "url": "/front-end-clothing-company-site/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "48ba27dad61e598d6f4a",
+    "url": "/front-end-clothing-company-site/static/css/main.08016461.chunk.css"
+  },
+  {
+    "revision": "2aeee8a1f69c1e93e28d",
+    "url": "/front-end-clothing-company-site/static/js/2.337efa97.chunk.js"
+  },
+  {
+    "revision": "d20ac35e665b22e2150f751ae27eac10",
+    "url": "/front-end-clothing-company-site/static/js/2.337efa97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ba27dad61e598d6f4a",
+    "url": "/front-end-clothing-company-site/static/js/main.3340c410.chunk.js"
+  },
+  {
+    "revision": "0f171385cc5451088cf0",
+    "url": "/front-end-clothing-company-site/static/js/runtime-main.d8c5f4d1.js"
+  },
+  {
+    "revision": "30ca118061cf342d26e38316a6a45a75",
+    "url": "/front-end-clothing-company-site/static/media/D-CHOICE-15-resized.30ca1180.jpg"
+  },
+  {
+    "revision": "e7dc7ea6600f4f887a4afe12e8861f0c",
+    "url": "/front-end-clothing-company-site/static/media/D-CHOICE-15.e7dc7ea6.jpg"
+  },
+  {
+    "revision": "fcb6f69c0d70c1937b8c87371497b798",
+    "url": "/front-end-clothing-company-site/static/media/D-CHOICE-19-resized.fcb6f69c.jpg"
+  },
+  {
+    "revision": "417d276abbcd20d6b3b0fba29c32114c",
+    "url": "/front-end-clothing-company-site/static/media/D-CHOICE-19.417d276a.jpg"
+  },
+  {
+    "revision": "3aa3b47ad54e1563b88aad29efba39e0",
+    "url": "/front-end-clothing-company-site/static/media/D-CHOICE-2-resized.3aa3b47a.jpg"
+  },
+  {
+    "revision": "680a39e48399e6225f01505557162d14",
+    "url": "/front-end-clothing-company-site/static/media/D-CHOICE-2.680a39e4.jpg"
+  },
+  {
+    "revision": "a1dfa17d35733af85592bb2ed930e8c4",
+    "url": "/front-end-clothing-company-site/static/media/D-CHOICE-6-resized.a1dfa17d.jpg"
+  },
+  {
+    "revision": "03c07284a7b03e2da8804f9e1a3b3717",
+    "url": "/front-end-clothing-company-site/static/media/D-CHOICE-6.03c07284.jpg"
+  },
+  {
+    "revision": "387a66a9e731bf1caf7f462dd9bb24c4",
+    "url": "/front-end-clothing-company-site/static/media/exit-sign.387a66a9.jpg"
+  },
+  {
+    "revision": "b35a0d1981b702082e76804397b62f1d",
+    "url": "/front-end-clothing-company-site/static/media/gallery-background.b35a0d19.jpg"
+  },
+  {
+    "revision": "9aa23576b32cd7c75b8650bb23aaf40e",
+    "url": "/front-end-clothing-company-site/static/media/harleys.9aa23576.jpg"
+  },
+  {
+    "revision": "5151afd1b575b59c54f2c5d30ef6f17d",
+    "url": "/front-end-clothing-company-site/static/media/product-pic1.5151afd1.jpg"
+  },
+  {
+    "revision": "6832d87ea956f7ac89dc17215fe44d08",
+    "url": "/front-end-clothing-company-site/static/media/product-pic2.6832d87e.jpg"
+  },
+  {
+    "revision": "08f99cc51b1614fbdbebab11a7e71f28",
+    "url": "/front-end-clothing-company-site/static/media/tee.08f99cc5.jpg"
+  },
+  {
+    "revision": "90bf459e8bd168ce16623be5555538c0",
+    "url": "/front-end-clothing-company-site/static/media/tee2.90bf459e.jpg"
+  }
+]);
